fb866f98ecc764387cbe2735d33f3c4c0e307fb2
Ramraj02 <rammba007@gmail.com>
2017-04-14 22:03:01 +0530

    First commit

---
+++ VariableDeclaration.java
@@ -0,0 +1,13 @@
+
+public class VariableDeclaration {
+	static int a=10;
+	int b;
+	public static void main (String [] args)
+	{
+		int c=30;
+		System.out.println(a);
+		VariableDeclaration a=new VariableDeclaration();
+		System.out.println(a.b);
+	}
+
+}
